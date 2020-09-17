ec6155eca4f207b969601813fe22370784ce79e0
jrarkaan <rakajanitraa@hotmail.com>
2020-09-17 10:36:37 +0700

    menambahkan file baru; infoBox dan mengkodingnya. dan mengimport di App.js

---
+++ src/infoBox.js
@@ -0,0 +1,20 @@
+import React, { Component, useState, useEffect } from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+const InfoBox = ({ title, cases, total })=>{
+  return(
+    <Card>
+      <CardContent className="infoBox">
+        {/* Title i.e Coronavirus cases */}
+        <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
+        {/* +120k number of cases */}
+        <h2 className="infoBox__cases">{cases}</h2>
+        {/* 1.2M Total */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+export default InfoBox;
